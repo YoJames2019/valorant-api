69636d973463fbbb3bf43c7891add82e89e6e524
James Clark <jamesclark1700@gmail.com>
2022-05-07 09:52:36 -0400

    small change to how you set customgame rules for convenience

--- src/GenericClient.js
+++ src/GenericClient.js
@@ -1,20 +1,20 @@
-const axios = require('axios');
-const https = require('https');
-const SocksAgent = require('axios-socks5-agent');
+const axios = require("axios");
+const https = require("https");
+const SocksAgent = require("axios-socks5-agent");
 
 const ciphers = [
-    'TLS_CHACHA20_POLY1305_SHA256',
-    'TLS_AES_128_GCM_SHA256',
-    'TLS_AES_256_GCM_SHA384',
-    'TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256'
+  "TLS_CHACHA20_POLY1305_SHA256",
+  "TLS_AES_128_GCM_SHA256",
+  "TLS_AES_256_GCM_SHA384",
+  "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
 ];
 
 class GenericClient {
-  async fetch(endpoint, endpointType, method = 'GET', data = null) {
+  async fetch(endpoint, endpointType, method = "GET", data = null) {
     try {
       var response, axiosEndpoint, axiosOptions;
 
-      if (endpointType == 'local') {
+      if (endpointType == "local") {
         axiosEndpoint = `${this._lockfile.protocol}://127.0.0.1:${this._lockfile.port}${endpoint}`;
         axiosHeaders = this.localHeaders;
         axiosOptions = {
@@ -22,39 +22,39 @@ class GenericClient {
           headers: this.localHeaders,
           httpsAgent: new https.Agent({
             rejectUnauthorized: false,
-            ciphers: ciphers.join(':'),
+            ciphers: ciphers.join(":"),
             honorCipherOrder: true,
-            minVersion: 'TLSv1.2'
+            minVersion: "TLSv1.2",
           }),
-          data: data
+          data: data,
         };
-      } else if (endpointType == 'glz') {
+      } else if (endpointType == "glz") {
         axiosEndpoint = `https://glz-${this._region}-1.${this._region}.a.pvp.net${endpoint}`;
         axiosOptions = {
           method: method.toUpperCase(),
           headers: this.remoteHeaders,
           data: data,
           httpsAgent: new https.Agent({
-            ciphers: ciphers.join(':'),
+            ciphers: ciphers.join(":"),
             honorCipherOrder: true,
-            minVersion: 'TLSv1.2'
-          })
+            minVersion: "TLSv1.2",
+          }),
         };
-      } else if (endpointType == 'pd') {
+      } else if (endpointType == "pd") {
         axiosEndpoint = `https://pd.${this._region}.a.pvp.net${endpoint}`;
         axiosOptions = {
           method: method.toUpperCase(),
           headers: this.remoteHeaders,
           data: data,
           httpsAgent: new https.Agent({
-            ciphers: ciphers.join(':'),
+            ciphers: ciphers.join(":"),
             honorCipherOrder: true,
-            minVersion: 'TLSv1.2'
-          })
+            minVersion: "TLSv1.2",
+          }),
         };
       }
 
-      if (this.proxyEnabled && endpointType != 'local') {
+      if (this.proxyEnabled && endpointType != "local") {
         axiosOptions.httpAgent = this.proxyAgents.httpAgent;
         axiosOptions.httpsAgent = this.proxyAgents.httpsAgent;
       }
@@ -77,187 +77,261 @@ class GenericClient {
     this.proxyAgents = new SocksAgent({
       host: proxyHost,
       port: proxyPort,
-      agentOptions: { keepAlive: true, rejectUnauthorized: false }
+      agentOptions: { keepAlive: true, rejectUnauthorized: false },
     });
   }
 
   async fetchAccountXP() {
-    return await this.fetch(`/account-xp/v1/players/${this.puuid}`, 'pd');
+    return await this.fetch(`/account-xp/v1/players/${this.puuid}`, "pd");
   }
 
   async fetchContracts() {
-    return await this.fetch(`/contracts/v1/contracts/${this.puuid}`, 'pd');
+    return await this.fetch(`/contracts/v1/contracts/${this.puuid}`, "pd");
   }
 
   async fetchContractDefinitions() {
-    return await this.fetch('/contract-definitions/v2/definitions', 'pd');
+    return await this.fetch("/contract-definitions/v2/definitions", "pd");
   }
 
   async fetchContractDefinitionsItemUpgrades() {
-    return await this.fetch('/contract-definitions/v3/item-upgrades', 'pd');
+    return await this.fetch("/contract-definitions/v3/item-upgrades", "pd");
   }
 
   async activateContract(contractId) {
-    return await this.fetch(`/contracts/v1/contracts/${this.puuid}/special/${contractId}`, 'pd');
+    return await this.fetch(
+      `/contracts/v1/contracts/${this.puuid}/special/${contractId}`,
+      "pd"
+    );
   }
 
   async fetchActiveBattlePass() {
-    return await this.fetch('/contract-definitions/v2/definitions/story', 'pd');
+    return await this.fetch("/contract-definitions/v2/definitions/story", "pd");
   }
 
   async coregameFetchPlayer() {
-    return await this.fetch(`/core-game/v1/players/${this.puuid}`, 'glz');
+    return await this.fetch(`/core-game/v1/players/${this.puuid}`, "glz");
   }
 
   async coregameFetchMatch(matchId) {
-    return await this.fetch(`/core-game/v1/matches/${matchId}`, 'glz');
+    return await this.fetch(`/core-game/v1/matches/${matchId}`, "glz");
   }
 
   async coregameFetchMatchLoadouts(matchId) {
-    return await this.fetch(`/core-game/v1/matches/${matchId}/loadouts`, 'glz');
+    return await this.fetch(`/core-game/v1/matches/${matchId}/loadouts`, "glz");
   }
 
   async coregameDisassociatePlayer(matchId) {
-    return await this.fetch(`/core-game/v1/players/${this.puuid}/disassociate/${matchId}`, 'glz');
+    return await this.fetch(
+      `/core-game/v1/players/${this.puuid}/disassociate/${matchId}`,
+      "glz"
+    );
   }
 
   async fetchMatchDetails(matchId) {
-    return await this.fetch(`/match-details/v1/matches/${matchId}`, 'pd');
+    return await this.fetch(`/match-details/v1/matches/${matchId}`, "pd");
   }
 
   async fetchMatchHistory(puuid, startIndex = 0, endIndex = 15, queueId) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/match-history/v1/history/${puuid}?startIndex=${startIndex}&endIndex=${endIndex}` + (queueId ? `&queue=${queueId}` : ''), 'pd');
+    return await this.fetch(
+      `/match-history/v1/history/${puuid}?startIndex=${startIndex}&endIndex=${endIndex}` +
+        (queueId ? `&queue=${queueId}` : ""),
+      "pd"
+    );
   }
 
   async fetchMMR(puuid) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/mmr/v1/players/${puuid}`, 'pd');
+    return await this.fetch(`/mmr/v1/players/${puuid}`, "pd");
   }
 
   async fetchCompetitiveUpdates(puuid, startIndex = 0, endIndex = 15, queueId) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/mmr/v1/players/${puuid}/competitiveupdates?startIndex=${startIndex}&endIndex=${endIndex}` + (queueId ? `&queue=${queueId}` : ''), 'pd');
+    return await this.fetch(
+      `/mmr/v1/players/${puuid}/competitiveupdates?startIndex=${startIndex}&endIndex=${endIndex}` +
+        (queueId ? `&queue=${queueId}` : ""),
+      "pd"
+    );
   }
 
   async fetchLeaderboard(seasonId, startIndex = 0, size = 1000) {
-    return await this.fetch(`/mmr/v1/leaderboards/affinity/${this._region}/queue/competitive/season/${seasonId}?startIndex=${startIndex}&size=${size}`, 'pd');
+    return await this.fetch(
+      `/mmr/v1/leaderboards/affinity/${this._region}/queue/competitive/season/${seasonId}?startIndex=${startIndex}&size=${size}`,
+      "pd"
+    );
   }
 
   async fetchPlayerLoadout(puuid) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/personalization/v2/players/${puuid}/playerloadout`, 'pd');
+    return await this.fetch(
+      `/personalization/v2/players/${puuid}/playerloadout`,
+      "pd"
+    );
   }
 
   async fetchPartyFromPuuid(puuid) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/parties/v1/players/${puuid}`, 'glz');
+    return await this.fetch(`/parties/v1/players/${puuid}`, "glz");
   }
 
   async fetchPartyFromPartyId(partyId) {
-    return await this.fetch(`/parties/v1/parties/${partyId}`, 'glz');
+    return await this.fetch(`/parties/v1/parties/${partyId}`, "glz");
   }
 
   async fetchPartyCustomGameConfigs() {
-    return await this.fetch(`/parties/v1/parties/customgameconfigs`, 'glz');
+    return await this.fetch(`/parties/v1/parties/customgameconfigs`, "glz");
   }
 
   async enterMatchmakingQueue(partyId) {
-    return await this.fetch(`/parties/v1/parties/${partyId}/matchmaking/join`, 'glz', 'post');
+    return await this.fetch(
+      `/parties/v1/parties/${partyId}/matchmaking/join`,
+      "glz",
+      "post"
+    );
   }
 
-  async setPartyAccessibility(partyId, state = 'OPEN') {
-    return await this.fetch(`/parties/v1/parties/${partyId}/accessibility`, 'glz', 'post', {
-      'Accessibility': state.toUpperCase()
-    })
+  async setPartyAccessibility(partyId, state = "OPEN") {
+    return await this.fetch(
+      `/parties/v1/parties/${partyId}/accessibility`,
+      "glz",
+      "post",
+      {
+        Accessibility: state.toUpperCase(),
+      }
+    );
   }
 
-  async setPartyQueue(partyId, queueId = 'unrated') {
-    return await this.fetch(`/parties/v1/parties/${partyId}/makedefault?queueID=${queueId}`, 'glz', 'post');
+  async setPartyQueue(partyId, queueId = "unrated") {
+    return await this.fetch(
+      `/parties/v1/parties/${partyId}/makedefault?queueID=${queueId}`,
+      "glz",
+      "post"
+    );
   }
 
   async makeCustomGame(partyId) {
-    return await this.fetch(`/parties/v1/parties/${partyId}/makecustomgame`, 'glz', 'post');
+    return await this.fetch(
+      `/parties/v1/parties/${partyId}/makecustomgame`,
+      "glz",
+      "post"
+    );
   }
 
-  async setCustomGameSettings(partyId, map = 'Ascent', mode = '/Game/GameModes/Bomb/BombGameMode.BombGameMode_C', server = 'aresriot.aws-rclusterprod-use1-1.na-gp-ashburn-1', rules = {}) {
-    return await this.fetch(`/parties/v1/parties/${partyId}/customgamesettings`, 'glz', 'post', {
-      'map': `/Game/Maps/${map}/${map}`,
-      'Mode': mode,
-      'GamePod': server,
-      'GameRules': rules
-    });
+  async setCustomGameSettings(
+    partyId,
+    map = "Ascent",
+    rules = {},
+    mode = "/Game/GameModes/Bomb/BombGameMode.BombGameMode_C",
+    server = "aresriot.aws-rclusterprod-use1-1.na-gp-ashburn-1"
+  ) {
+    return await this.fetch(
+      `/parties/v1/parties/${partyId}/customgamesettings`,
+      "glz",
+      "post",
+      {
+        map: `/Game/Maps/${map}/${map}`,
+        Mode: mode,
+        GamePod: server,
+        GameRules: rules,
+      }
+    );
   }
 
   async joinParty(partyId) {
-    return await this.fetch(`/parties/v1/players/${this.puuid}/joinparty/${partyId}`, 'glz', 'post');
+    return await this.fetch(
+      `/parties/v1/players/${this.puuid}/joinparty/${partyId}`,
+      "glz",
+      "post"
+    );
   }
 
   async leaveParty(partyId) {
-    return await this.fetch(`/parties/v1/players/${this.puuid}/leaveparty/${partyId}`, 'glz', 'post');
+    return await this.fetch(
+      `/parties/v1/players/${this.puuid}/leaveparty/${partyId}`,
+      "glz",
+      "post"
+    );
   }
 
   async inviteToPartyByDisplayName(partyId, username, tag) {
-    return await this.fetch(`/parties/v1/parties/${partyId}/invites/name/${username}/tag/${tag}`, 'glz', 'post')
+    return await this.fetch(
+      `/parties/v1/parties/${partyId}/invites/name/${username}/tag/${tag}`,
+      "glz",
+      "post"
+    );
   }
 
   async pregameFetchPlayer(puuid) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/pregame/v1/players/${puuid}`, 'glz');
+    return await this.fetch(`/pregame/v1/players/${puuid}`, "glz");
   }
 
   async pregameFetchMatch(matchId) {
-    return await this.fetch(`/pregame/v1/matches/${matchId}`, 'glz');
+    return await this.fetch(`/pregame/v1/matches/${matchId}`, "glz");
   }
 
   async pregameFetchLoadouts(matchId) {
-    return await this.fetch(`/pregame/v1/matches/${matchId}/loadouts`, 'glz');
+    return await this.fetch(`/pregame/v1/matches/${matchId}/loadouts`, "glz");
   }
 
   async selectCharacter(matchId, characterId) {
-    return await this.fetch(`/pregame/v1/matches/${matchId}/select/${characterId}`, 'glz', 'post');
+    return await this.fetch(
+      `/pregame/v1/matches/${matchId}/select/${characterId}`,
+      "glz",
+      "post"
+    );
   }
 
   async lockCharacter(matchId, characterId) {
-    return await this.fetch(`/pregame/v1/matches/${matchId}/lock/${characterId}`, 'glz', 'post');
+    return await this.fetch(
+      `/pregame/v1/matches/${matchId}/lock/${characterId}`,
+      "glz",
+      "post"
+    );
   }
 
   async fetchPenalties() {
-    return await this.fetch('/restrictions/v2/penalties', 'pd');
+    return await this.fetch("/restrictions/v2/penalties", "pd");
   }
 
   async fetchSession(puuid) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/session/v1/sessions/${puuid}`, 'glz');
+    return await this.fetch(`/session/v1/sessions/${puuid}`, "glz");
   }
 
-  async fetchStoreEntitlements(puuid, itemType = 'e7c63390-eda7-46e0-bb7a-a6abdacd2433') {
+  async fetchStoreEntitlements(
+    puuid,
+    itemType = "e7c63390-eda7-46e0-bb7a-a6abdacd2433"
+  ) {
     puuid = puuid ? puuid : this.puuid;
 
-    return await this.fetch(`/store/v1/entitlements/${puuid}/${itemType}`, 'pd');
+    return await this.fetch(
+      `/store/v1/entitlements/${puuid}/${itemType}`,
+      "pd"
+    );
   }
 
   async fetchStoreOffers() {
-    return await this.fetch(`/store/v1/offers/`, 'pd');
+    return await this.fetch(`/store/v1/offers/`, "pd");
   }
 
   async fetchWallet() {
-    return await this.fetch(`/store/v1/wallet/${this.puuid}`, 'pd');
+    return await this.fetch(`/store/v1/wallet/${this.puuid}`, "pd");
   }
 
   async fetchStoreFront() {
-    return await this.fetch(`/store/v2/storefront/${this.puuid}`, 'pd');
+    return await this.fetch(`/store/v2/storefront/${this.puuid}`, "pd");
   }
 
   async fetchUsernamesFromPuuids(ids = []) {
-    return await this.fetch('/name-service/v2/players', 'pd', 'put', ids);
+    return await this.fetch("/name-service/v2/players", "pd", "put", ids);
   }
 }
 
