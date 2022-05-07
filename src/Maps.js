bba4acbc7558ce9cd3e83b6568bf681cbea7ca93
James Clark <jamesclark1700@gmail.com>
2022-05-07 09:43:35 -0400

    add map name to codename translation key

---
+++ src/Maps.js
@@ -0,0 +1,11 @@
+const Maps = {
+  Ascent: "Ascent",
+  Bind: "Duality",
+  Breeze: "Foxtrot",
+  Icebox: "Port",
+  Haven: "Triad",
+  Split: "Bonsai",
+  Fracture: "Canyon",
+};
+
+module.exports = Maps;
